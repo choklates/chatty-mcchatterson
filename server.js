975ec6eb0d5c04be746a79de4c0c0c87e9245ac5
Victor Hoang <vic.hoang@yahoo.com>
2015-10-18 16:08:27 -0700

    Update port to use process.env.PORT

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
@@ -25,6 +27,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
-  console.log('listening on *:3000');
+http.listen(app.get('port'), function() {
+  console.log('listening on', app.get('port'));
 });
