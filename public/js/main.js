3579268709b4c95b9689a929a52e1b5cb39c8422
Victor Hoang <vic.hoang@yahoo.com>
2015-10-18 16:57:08 -0700

    Add opinionated styling to HTML, CSS, and JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,20 @@
 'use strict';
 
-var socket = io();
+var socket,
+  $messageList, $messageForm, $messageInput;
 
-$('form').on('submit', function(e) {
+socket = io();
+$messageList = $('.js-message-list');
+$messageForm = $('.js-message-form');
+$messageInput = $messageForm.find('.js-input');
+
+$messageForm.on('submit', function(e) {
   e.preventDefault();
 
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
+  socket.emit('chat message', $messageInput.val());
+  $messageInput.val('');
 });
 
 socket.on('chat message', function(message) {
-  $('#messages').append($('<li>').text(message));
+  $messageList.append($('<li>').addClass('message').text(message));
 });
