a1c5ef3c255a82d11abd98c7aacf2f6624db62bc
Victor Hoang <vic.hoang@yahoo.com>
2015-10-19 00:16:00 -0700

    Use var port in server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
+var port    = process.env.PORT || 5000;
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', port);
 
 app.use(express.static('public'));
 
@@ -27,6 +28,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(app.get('port'), function() {
-  console.log('listening on', app.get('port'));
+http.listen(port, function() {
+  console.log('listening on', port);
 });
